1b4262ff4f743de49282c6692500548119132ca0
winterwind999 <realjordanfaciol@gmail.com>
2026-01-11 02:44:25 +0800

    updated experiences

--- data/experiences.ts
+++ data/experiences.ts
@@ -10,7 +10,7 @@ export const EXPERIENCES: Experience[] = [
         descriptions: [
           "A web-based system designed to streamline the management of APE, PEME, Executive, and Diagnostic medical examinations.",
           "Built with Typescript, React, Node.js, Express.js, MySQL, Tailwind CSS, TanStack Query, Axios, Zustand, REST API, and Socket.io",
-          "Key features include pre-filled PDF generation, CSV parsing, notifications, data analytics, a dynamic form builder, data versioning, and file encryption, decryption, viewing, and upload to cloudinary",
+          "Key features include pre-filled PDF generation, CSV parsing, notifications, data analytics, a dynamic form builder, data versioning, and file encryption, decryption, viewing, and upload to Cloudinary",
         ],
       },
       {
@@ -18,7 +18,7 @@ export const EXPERIENCES: Experience[] = [
         descriptions: [
           "A web application that enables HR staff to easily receive and view employees’ medical test results.",
           "Built with Typescript, React, Node.js, Express.js, MySQL, Tailwind CSS, TanStack Query, Axios, Zustand, and REST API",
-          "Supports secure viewing of uploaded medical documents and files stored in cloudinary.",
+          "Supports secure viewing of uploaded medical documents and files stored in Cloudinary.",
         ],
       },
     ],
